4c71dd9459e8d018619f000c686ecd64a782a526
Moiseev Ilya <mois.ilya@gmail.com>
2023-04-21 22:10:02 +0400

    First request rewrited on mongo

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ChatsController } from './chats.controller';
 import { ChatsService } from './chats.service';
+import { MongoClient } from 'mongodb';
 
 @Module({
   controllers: [ChatsController],
-  providers: [ChatsService],
+  providers: [
+    ChatsService,
+    {
+      provide: 'DATABASE_CONNECTION',
+      useFactory: async () => {
+        const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
+        await client.connect();
+        return client;
+      },
+    },
+  ],
 })
 export class ChatsModule {}
--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,22 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { ChatDto } from './chat.dto';
 import { MessageDto } from './message.dto';
+import { Db, MongoClient } from 'mongodb';
 
-import Datastore from '@seald-io/nedb';
+// import Datastore from '@seald-io/nedb';
 import path from 'path';
 
 @Injectable()
 export class ChatsService {
-  async getChats(): Promise<ChatDto[]> {
-    const databasePath = path.join(process.env.DATABASE_PATH, 'chats.db');
-    const db = new Datastore({
-      filename: databasePath,
-      autoload: true,
-    });
+  private db: Db;
 
-    const docs = await db
-      .findAsync({}, { id: 1, title: 1, last_message: 1 })
+  constructor(@Inject('DATABASE_CONNECTION') private dbClient: MongoClient) {
+    this.db = this.dbClient.db('thistory');
+  }
+
+  async getChats(): Promise<ChatDto[]> {
+    const collection = await this.db.collection('chats');
+
+    const cursor = collection
+      .find({})
+      .project({ id: 1, title: 1, last_message: 1 })
       .sort({ 'last_message.date': -1 });
+    const docs = await cursor.toArray();
 
     const transformedDocs = docs.map((doc) => {
       const lastMessage = {
@@ -38,32 +43,33 @@ export class ChatsService {
 
   async getChatById(chatId: number): Promise<ChatDto> | undefined {
     const databasePath = path.join(process.env.DATABASE_PATH, 'chats.db');
-    const db = new Datastore({
-      filename: databasePath,
-      autoload: true,
-    });
+    return;
+    // const db = new Datastore({
+    //   filename: databasePath,
+    //   autoload: true,
+    // });
 
-    const doc = await db.findOneAsync(
-      { _id: chatId },
-      { id: 1, title: 1, last_message: 1 },
-    );
+    // const doc = await db.findOneAsync(
+    //   { _id: chatId },
+    //   { id: 1, title: 1, last_message: 1 },
+    // );
 
-    if (!doc) {
-      return;
-    }
+    // if (!doc) {
+    //   return;
+    // }
 
-    const lastMessage = {
-      id: doc.last_message.id,
-      sender: doc.last_message.sender_id?.user_id,
-      content: doc.last_message?.content?.text?.text,
-      unixtime: doc.last_message?.date,
-    };
+    // const lastMessage = {
+    //   id: doc.last_message.id,
+    //   sender: doc.last_message.sender_id?.user_id,
+    //   content: doc.last_message?.content?.text?.text,
+    //   unixtime: doc.last_message?.date,
+    // };
 
-    return {
-      id: doc.id,
-      title: doc.title,
-      lastMessage,
-    };
+    // return {
+    //   id: doc.id,
+    //   title: doc.title,
+    //   lastMessage,
+    // };
   }
 
   async getMessagesByChatId(
@@ -71,42 +77,43 @@ export class ChatsService {
     fromMessageId: number,
     limit: number,
   ): Promise<MessageDto[]> | undefined {
-    const databasePath = path.join(
-      process.env.DATABASE_PATH,
-      'chats',
-      `${chatId}.db`,
-    );
-    const db = new Datastore({
-      filename: databasePath,
-      autoload: true,
-    });
+    return;
+    // const databasePath = path.join(
+    //   process.env.DATABASE_PATH,
+    //   'chats',
+    //   `${chatId}.db`,
+    // );
+    // const db = new Datastore({
+    //   filename: databasePath,
+    //   autoload: true,
+    // });
 
-    const query = fromMessageId === 0 ? {} : { id: { $lt: fromMessageId } };
+    // const query = fromMessageId === 0 ? {} : { id: { $lt: fromMessageId } };
 
-    const docs = await db
-      .findAsync(query)
-      .projection({
-        id: 1,
-        'sender_id.user_id': 1,
-        'content.text.text': 1,
-        date: 1,
-      })
-      .limit(limit)
-      .sort({ date: -1 });
+    // const docs = await db
+    //   .findAsync(query)
+    //   .projection({
+    //     id: 1,
+    //     'sender_id.user_id': 1,
+    //     'content.text.text': 1,
+    //     date: 1,
+    //   })
+    //   .limit(limit)
+    //   .sort({ date: -1 });
 
-    const transformedDocs = docs
-      .map((doc) => {
-        const text = doc.content?.text?.text;
+    // const transformedDocs = docs
+    //   .map((doc) => {
+    //     const text = doc.content?.text?.text;
 
-        return {
-          id: doc.id,
-          sender: doc.sender_id.user_id,
-          content: text,
-          unixtime: doc.date,
-        };
-      })
-      .reverse();
+    //     return {
+    //       id: doc.id,
+    //       sender: doc.sender_id.user_id,
+    //       content: text,
+    //       unixtime: doc.date,
+    //     };
+    //   })
+    //   .reverse();
 
-    return transformedDocs;
+    // return transformedDocs;
   }
 }
